e7ea81704738aa90814591f56a478b7b33889cdf
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-23 14:47:23 +0700

    Initial commit

---
+++ lib/WordList/ID/Fruit/PERLANCAR.pm
@@ -0,0 +1,165 @@
+package WordList::ID::Fruit::PERLANCAR;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: List of fruit names in Indonesian
+
+=head1 DESCRIPTION
+
+Suitable for playing hangman.
+
+Some spelling variations included (e.g. C<ceremai> and C<cermai>, C<nenas> and
+C<nanas>, C<mentimun> and C<timun>). Multiple popular names for the same fruit
+included (e.g. C<jeruk bali> and C<pomelo>, C<frambozen> and C<raspberry>).
+
+=head1 SEE ALSO
+
+=cut
+
+__DATA__
+almond
+alpukat
+anggur
+apel
+aprikot
+ara
+arbei
+aren
+asam
+atap
+bacang
+badam
+belimbing
+belimbing sayur
+benda
+bengkuang
+beri
+binjai
+bisbul
+bit
+blackberry
+blackcurrant
+blewah
+bluberi
+buni
+cempaka
+cempedak
+ceremai
+ceri
+cermai
+coklat
+cranberry
+delima
+duku
+duren
+durian
+duwet
+enau
+erbis
+fragaria
+frambozen
+gandaria
+gowok
+guava
+jagung
+jamblang
+jambu
+jambu air
+jambu batu
+jambu biji
+jambu bol
+jambu mete
+jambu monyet
+jeruk
+jeruk bali
+jeruk limau
+jeruk mandarin
+jeruk nipis
+jujuba
+jujube
+kakao
+kapulasan
+kawista
+kecapi
+kedondong
+kelapa
+kelapa kopyor
+kelengkeng
+kemang
+kemiri
+kenari
+kepa
+kepel
+kersen
+kesemek
+kiwi
+kopyor
+kumkuat
+kupa
+kurma
+kweni
+leci
+lemon
+lengkeng
+limau
+maja
+malaka
+mandarin
+mangga
+manggis
+markisa
+matoa
+melon
+mengkudu
+mentawa
+menteng
+mentimun
+mentimun suri
+murbai
+naga
+namnam
+nanas
+nangka
+nenas
+nona
+pepaya
+persik
+pinang
+pir
+pisang
+plum
+pomelo
+prem
+rambai
+rambusa
+rambutan
+raspberry
+rukam
+salak
+sawit
+sawo
+semangka
+simpur
+sirsak
+srikaya
+stroberi
+sukun
+terong
+timun
+timun suri
+tin
+tomat
+ubi
+vanili
+waluh
+widuri
+zaitun
